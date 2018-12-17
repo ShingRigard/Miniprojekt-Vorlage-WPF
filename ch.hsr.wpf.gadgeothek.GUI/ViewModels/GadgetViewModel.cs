de32fd238209d22474830a444dbc7c2103e5ac5f
MMSprecher <mmsprecher@gmail.com>
2018-12-17 13:13:01 +0100

    ViewModels created

---
+++ ch.hsr.wpf.gadgeothek.GUI/ViewModels/GadgetViewModel.cs
@@ -0,0 +1,70 @@
+﻿using ch.hsr.wpf.gadgeothek.domain;
+using ch.hsr.wpf.gadgeothek.service;
+
+namespace ch.hsr.wpf.gadgeothek.GUI.ViewModels
+{
+    public class GadgetViewModel
+    {
+        private readonly Gadget _gadget;
+        private readonly LibraryAdminService _service;
+
+        public GadgetViewModel(Gadget gadget, LibraryAdminService service)
+        {
+            _service = service;
+            _gadget = gadget;
+        }
+
+        public Gadget GetGadget()
+        {
+            return _gadget;
+        }
+
+        public string InventoryNumber => _gadget.InventoryNumber;
+
+        public string Manufacturer
+        {
+            get => _gadget.Manufacturer;
+
+            set
+            {
+                _gadget.Manufacturer = value;
+                _service.UpdateGadget(_gadget);
+            }
+        }
+
+        public string Name
+        {
+            get => _gadget.Name;
+            set
+            {
+                _gadget.Name = value;
+                _service.UpdateGadget(_gadget);
+            }
+        }
+
+        public double Price
+        {
+            get => _gadget.Price;
+            set
+            {
+                _gadget.Price = value;
+                _service.UpdateGadget(_gadget);
+            }
+        }
+
+        public Condition Condition
+        {
+            get => _gadget.Condition;
+            set
+            {
+                _gadget.Condition = value;
+                _service.UpdateGadget(_gadget);
+            }
+        }
+
+        public bool Remove()
+        {
+            return _service.DeleteGadget(_gadget);
+        }
+    }
+}
---
+++ ch.hsr.wpf.gadgeothek.GUI/ViewModels/ReservationViewModel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using ch.hsr.wpf.gadgeothek.domain;
+using ch.hsr.wpf.gadgeothek.service;
+
+namespace ch.hsr.wpf.gadgeothek.GUI.ViewModels
+{
+    public class ReservationViewModel
+    {
+        private readonly Reservation _reservation;
+        private readonly LibraryAdminService _service;
+
+        public ReservationViewModel(Reservation reservation, LibraryAdminService service)
+        {
+            _reservation = reservation;
+            _service = service;
+        }
+
+        public string Id => _reservation.Id;
+
+        public string CustomerId
+        {
+            get => _reservation.CustomerId;
+            set
+            {
+                _reservation.CustomerId = value;
+                _service.UpdateReservation(_reservation);
+            }
+        }
+
+        public Customer Customer
+        {
+            get => _reservation.Customer;
+            set
+            {
+                _reservation.Customer = value;
+                _service.UpdateReservation(_reservation);
+            }
+        }
+
+        public string GadgetId
+        {
+            get => _reservation.GadgetId;
+            set
+            {
+                _reservation.GadgetId = value;
+                _service.UpdateReservation(_reservation);
+            }
+        }
+
+        public Gadget Gadget
+        {
+            get => _reservation.Gadget;
+            set
+            {
+                _reservation.Gadget = value;
+                _service.UpdateReservation(_reservation);
+            }
+        }
+
+        public DateTime? ReservationDate
+        {
+            get => _reservation.ReservationDate;
+            set
+            {
+                _reservation.ReservationDate = value;
+                _service.UpdateReservation(_reservation);
+            }
+        }
+
+        public bool Finished
+        {
+            get => _reservation.Finished;
+            set
+            {
+                _reservation.Finished = value;
+                _service.UpdateReservation(_reservation);
+            }
+        }
+
+        public int WaitingPosition
+        {
+            get => _reservation.WaitingPosition;
+            set
+            {
+                _reservation.WaitingPosition = value;
+                _service.UpdateReservation(_reservation);
+            }
+        }
+
+        public bool isReady
+        {
+            get => _reservation.IsReady;
+            set
+            {
+                _reservation.IsReady = value;
+                _service.UpdateReservation(_reservation);
+            }
+        }
+
+        public bool Remove()
+        {
+            return _service.DeleteReservation(_reservation);
+        }
+    }
+}
