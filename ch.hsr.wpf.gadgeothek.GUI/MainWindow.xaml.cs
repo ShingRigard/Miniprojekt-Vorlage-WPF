aedb0b11409f87b6ff3b962264ae8348ffb1d674
MMSprecher <mmsprecher@gmail.com>
2018-12-18 13:14:01 +0100

    fixed MainWindow

---
+++ ch.hsr.wpf.gadgeothek.GUI/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ch.hsr.wpf.gadgeothek.GUI.ViewModels;
+
+namespace ch.hsr.wpf.gadgeothek.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var vm = new MainWindowViewModel();
+            DataContext = vm;
+        }
+    }
+}
