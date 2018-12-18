a54844e1c2eefb1d04dea5cf4474fc8d18c174c0
Shing Rigard <johnny-luu@hotmail.com>
2018-12-18 17:07:03 +0100

    Bindung in MainWindow xml updated to a more acceptable standard.

---
+++ ch.hsr.wpf.gadgeothek.GUI/Validation/StringValidationRule.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace AutoReservation.GUI.Validation
+{
+    public class StringValidationRule : ValidationRule
+    {
+        public StringValidationRule()
+        {
+        }
+
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public bool CanBeEmpty { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string str = "";
+
+            if (!(value is string))
+            {
+                return new ValidationResult(false, "Value is not a string");
+            }
+            str = (string)value;
+
+            if (!CanBeEmpty && str.Length == 0)
+            {
+                return new ValidationResult(false, "Value can't be empty");
+            }
+            else if(str.Length < Min || Max > 0 && str.Length > Max)
+            {
+                return new ValidationResult(false, $"String length has to be in the range: {Min} - {Max}");
+            }
+
+            return new ValidationResult(true, null);
+        }
+    }
+}
